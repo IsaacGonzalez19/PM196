e75bb1ffb49650854570593c45cc35c70aa0f819
IsaacGonzalez19 <121037773@upq.edu.mx>
2025-05-16 17:39:09 -0600

    Problema B resuelto con lo que se pidio

---
+++ Practica1/ejercicio2.js
@@ -0,0 +1,20 @@
+//Ejercicio B
+
+/*Convierte esta función tradicional a una arrow function que haga exactamente lo
+mismo:
+*/
+
+//Esta function tiene la función de calcular el area de un cuadrado L x L
+/*
+function cuadrado(numero) {
+    return numero * numero;
+} */
+//Creamos la arrow function para calcular el area
+const cuadrado = (numero) => numero * numero;
+
+//Muestre en consola el resultado de 3 llamadas a la función/
+console.log(cuadrado(5));
+console.log(cuadrado(10));
+console.log(cuadrado(15));
+
+
