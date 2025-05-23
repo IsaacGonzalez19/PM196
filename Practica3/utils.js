c470fc887ba98d5c0a9dd6184e4ce080f11b79d1
JESSICA ARACELI GONZALEZ GONZALEZ <jessicaaraceligonzalezgonzalez.@MacBook-Air-de-JESSICA.local>
2025-05-23 16:53:13 -0600

    Se agrego la función de acuerdo a lo pedido

---
+++ Practica3/utils.js
@@ -0,0 +1,18 @@
+/*
+* a.
+1. Crea un archivo llamado utils.js con una función llamada restar(a, b) que retorne la resta
+de dos números.
+2. Crea un archivo principal (main.js), importa esa función y úsala ejecutar varias pruebas.
+*
+* */
+
+
+// Funcion the regresa una resta de dos numeros
+export function restar(a, b) {
+    return a - b;
+}
+
+
+
+
+
