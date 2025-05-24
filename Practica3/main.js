8f2bca5e9876becf11af9663896e5b70c458ef13
JESSICA ARACELI GONZALEZ GONZALEZ <jessicaaraceligonzalezgonzalez.@MacBook-Air-de-JESSICA.local>
2025-05-23 18:23:50 -0600

    Se agrego la función de acuerdo a lo pedido

--- Practica3/main.js
+++ Practica3/main.js
@@ -5,7 +5,7 @@
 import { restar } from './utils.js';
 
 // Prueba de la importación de la funcion  restar para diferentes casos.
-console.log('Test 1: 5 - 3 =', restar(5, 3));  // Prueba 1, resultado = 2
-console.log('Test 2: 10 - 7 =', restar(10, 7)); // Prueba 2, resultado 3
-console.log('Test 3: 0 - 5 =', restar(0, 5));   // Should output -5
-console.log('Test 4: -2 - (-8) =', restar(-2, -8)); // Should output 6
+console.log('Prueba 1: 5 - 3 =', restar(5, 3));  // Prueba 1, resultado = 2
+console.log('Prueba 2: 10 - 7 =', restar(10, 7)); // Prueba 2, resultado 3
+console.log('Prueba 3: 0 - 5 =', restar(0, 5));   // Should output -5
+console.log('Prueba 4: -2 - (-8) =', restar(-2, -8)); // Should output 6
