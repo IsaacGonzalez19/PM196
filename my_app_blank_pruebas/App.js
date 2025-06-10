46774747909cbb0b55ad4a5783fcbe6eebe2588c
IsaacGonzalez19 <121037773@upq.edu.mx>
2025-06-10 15:37:47 -0600

    Se ha agregado un leve funcionamiento en index.js

--- my_app_blank_pruebas/App.js
+++ my_app_blank_pruebas/App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto = () =>{
+  return(
+      <Text>Hola Mundo react Native</Text>
+  )
+}
+
 
 export default function App() {
   return (
+
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto> </Texto>
       <StatusBar style="auto" />
+      <Texto> </Texto>
+      <Button title="Presioname"> </Button>
+      <Texto> </Texto>
+
     </View>
   );
 }
