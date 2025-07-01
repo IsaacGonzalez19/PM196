5211548dfb1830554d9eb31e232cf452f0a201e1
IsaacGonzalez19 <121037773@upq.edu.mx>
2025-07-01 15:29:50 -0600

    Se agrego el archivo para mostrar en un ejemplo de SectionList y FlatList

--- practica_no_7_isaac_gonzalez/index.js
+++ practica_no_7_isaac_gonzalez/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './App_scrowview';
+import App from './App_flatlist';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
